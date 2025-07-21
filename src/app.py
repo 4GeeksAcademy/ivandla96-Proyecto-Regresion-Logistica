0b398bb4f6eefb24006b2d9eed5cf3d0b7b17e5b
Ivan Diaz <ivandla96@gmail.com>
2025-07-21 19:17:24 +0000

    Sin terminar

--- src/app.py
+++ src/app.py
@@ -2,3 +2,7 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import pandas as pd
+
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/logistic-regression-project-tutorial/main/bank-marketing-campaign-data.csv")
+total_data.head()
